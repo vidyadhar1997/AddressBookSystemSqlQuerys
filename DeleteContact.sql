eda7ad962d32ae63956c397c2164136554055c88
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-26 16:21:51 +0530

    [vidyadhar hudge]Add:-delete contacts using person name

---
+++ DeleteContact.sql
@@ -0,0 +1,4 @@
+delete from Address_Book where first_name='dhiraj';
+delete from Address_Book where first_name='suraj';
+delete from Address_Book where first_name='trimbak';
+delete from Address_Book where first_name='suresh';
