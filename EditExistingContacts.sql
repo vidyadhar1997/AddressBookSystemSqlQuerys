8103fce27b750f048c40ae473ab1cedceb11079a
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-26 16:16:22 +0530

    [vidyadhar hudge]Add:-edit existing contacts

---
+++ EditExistingContacts.sql
@@ -0,0 +1,3 @@
+update Address_Book set address='adarsh colny' where first_name='dhiraj';
+update Address_Book set city='delhi' where first_name='suraj';
+update Address_Book set state='kerala' where first_name='trimbak';
