0d1e9df1c50a572bf416211e47498ee42614c7c3
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-26 18:03:23 +0530

    [vidyadhar hudge]Add:-Add person to both friend and family

---
+++ AddPersonToBothFriendAndFamily.sql
@@ -0,0 +1,2 @@
+insert into Address_Book values ('nitikesh','hudge','tawarja colony','latur','maharashtra','413512','8149713160','dhiraj@gmail.com','family address book','Family'),
+('ramesh','hudge','tawarja colony','latur','maharashtra','413512','8149713160','dhiraj@gmail.com','friends address book','Friends');
