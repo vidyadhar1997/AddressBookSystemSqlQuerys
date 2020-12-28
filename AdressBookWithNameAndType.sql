c3445a2a219605eaa2641752710ba0e47dc86108
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-28 10:54:41 +0530

    [vidyadhar hudge]Refactor:Size of address book

--- AdressBookWithNameAndType.sql
+++ AdressBookWithNameAndType.sql
@@ -1,4 +1,4 @@
 alter table Address_Book add address_book_name varchar(50),address_book_type varchar(50);
 update Address_Book set address_book_name='family address book', address_book_type='Family' where first_name='dhiraj';
-update Address_Book set address_book_name='friends address book', address_book_type='Friends,' where first_name='vidyadhar';
+update Address_Book set address_book_name='friends address book', address_book_type='Friends' where first_name='vidyadhar';
 update Address_Book set address_book_name='profession address book', address_book_type='Profession' where first_name='suraj';
--- InsertNewContacts.sql
+++ InsertNewContacts.sql
@@ -1,8 +1,8 @@
 insert into Address_Book values('dhiraj','hudge','tawarja colony','latur','maharashtra','413512','8149713160','dhiraj@gmail.com')
 insert into Address_Book values('suraj','pawar','mataji negar','pune','karnataka','415673','8149613160','suraj@gmail.com')
-insert into Address_Book values('vidyadhar','hudge','bashweshwar','amravati','andhra','413678','8179713160','vidyadhar@gmail.com')
-insert into Address_Book values('trimbak','pawar','shivaji chowk','mumbai','up','748949','8149703160','trimbak@gmail.com')
-insert into Address_Book values('suresh','hudge','ashok hotel','udagir','rajshthan','413562','9149713160','suresh@gmail.com')
+insert into Address_Book values('dhiraj','hudge','bashweshwar','amravati','andhra','413678','8179713160','vidyadhar@gmail.com')
+insert into Address_Book values('suraj','pawar','shivaji chowk','amravati','up','748949','8149703160','trimbak@gmail.com')
+insert into Address_Book values('arav','hudge','ashok hotel','amravati','rajshthan','413562','9149713160','suresh@gmail.com')
 
 /*Displaying adress book*/
 select * from  Address_Book;
